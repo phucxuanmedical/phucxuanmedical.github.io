9baf2359a449f435d663a524d1c55780a6c6ac83
daongochieu2810 <daongochieu2810@yahoo.com.vn>
2020-10-08 18:20:58 +0800

    Updates

---
+++ precache-manifest.9698bc9a7afb4f39e322ca8c752580ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "488377caac7148ec2d12a084d08f3457",
+    "url": "/phucxuanweb/index.html"
+  },
+  {
+    "revision": "105a65b380cc44caf00f",
+    "url": "/phucxuanweb/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3c6d7e41651ca1209e77",
+    "url": "/phucxuanweb/static/js/2.946a6243.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/phucxuanweb/static/js/2.946a6243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "105a65b380cc44caf00f",
+    "url": "/phucxuanweb/static/js/main.47e2e4ec.chunk.js"
+  },
+  {
+    "revision": "a98fad790e7767bd96f0",
+    "url": "/phucxuanweb/static/js/runtime-main.f07cb012.js"
+  },
+  {
+    "revision": "66b35f6ae23a0325a32c0a9bdaa17246",
+    "url": "/phucxuanweb/static/media/aboutUs.66b35f6a.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4d327f15353fa3200b7e2bfe06c56fcb.js"
+  "/phucxuanweb/precache-manifest.9698bc9a7afb4f39e322ca8c752580ba.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/phucxuanweb/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
